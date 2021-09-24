2cbdd3b3bff7f8d2b61844b49408799dedacedd8
Manoj Soni <manojncsoni@gmail.com>
2021-09-24 11:14:16 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let bDate = document.querySelector('#b-date');
+let lNumber = document.querySelector('#lucky-no');
+let checkBtn = document.querySelector('#check-btn');
+let message = document.querySelector('.result');
+
+function displayMessage(msg) {
+    message.innerText = msg;
+}
+
+checkBtn.addEventListener('click', () => {
+
+    const date = bDate.value;
+    const number = lNumber.value;
+    
+   
+})
