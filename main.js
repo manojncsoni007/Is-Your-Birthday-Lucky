0727fac0128804fcb3b0611d7d8ee7fb6fb46aee
Manoj Soni <manojncsoni@gmail.com>
2021-09-25 00:01:27 +0530

    final push

--- main.js
+++ main.js
@@ -2,15 +2,39 @@ let bDate = document.querySelector('#b-date');
 let lNumber = document.querySelector('#lucky-no');
 let checkBtn = document.querySelector('#check-btn');
 let message = document.querySelector('.result');
+console.log('connected');
 
 function displayMessage(msg) {
     message.innerText = msg;
 }
 
-checkBtn.addEventListener('click', () => {
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
 
+function isNumberLucky(sumOfDate, numberCheck) {
+    if (sumOfDate % numberCheck === 0) {
+        return displayMessage(`${numberCheck} is lucky number !! 🥳`);
+    }
+
+    return displayMessage(`${numberCheck} is not that lucky 😕`)
+}
+
+checkBtn.addEventListener('click', () => {
     const date = bDate.value;
-    const number = lNumber.value;
-    displayMessage(number);
+    const numberCheck = lNumber.value;
+    const sumofDate = calculateSum(date);
+    if (date && numberCheck) {
+        isNumberLucky(sumofDate, numberCheck)
+    } else {
+        displayMessage('Enter the both fields *')
+    }
+
+
 
 })
